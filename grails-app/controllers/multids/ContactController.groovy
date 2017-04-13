a70859eccf7d17cf64cad8d8aff501a340bfec85
lucastex <lucas@blanq.com.br>
2017-04-12 22:00:32 -0300

    changed - controller structure to add and retrieve from different datasources

--- grails-app/controllers/multids/ContactController.groovy
+++ grails-app/controllers/multids/ContactController.groovy
@@ -2,9 +2,25 @@ package multids
 
 class ContactController {
 
+    def contactService
+
     def index() {
 
-        def contacts = Contact.findAll([sort:"name", order: "asc"])
-        [contacts: contacts]
+        def contactsDs = Contact.findAll([sort:"name", order: "asc"])
+        def contactsServerdb = Contact.serverdb.withTransaction { Contact.serverdb.findAll([sort:"name", order: "asc"]) }
+        def contactsUserdb = Contact.userdb.withTransaction { Contact.userdb.findAll([sort:"name", order: "asc"]) }
+        [contactsDs: contactsDs, contactsServerdb: contactsServerdb, contactsUserdb: contactsUserdb]
+    }
+
+    def add() {
+
+        def dbIndex = params.int('id') ?: ((int) (Math.random() * 3))
+        def contact = contactService.build()
+
+        if (dbIndex == 0) { Contact.withTransaction             { contact.save()            } }
+        if (dbIndex == 1) { Contact.serverdb.withTransaction    { contact.serverdb.save()   } }
+        if (dbIndex == 2) { Contact.userdb.withTransaction      { contact.userdb.save()     } }
+
+        redirect action: "index"
     }
 }
