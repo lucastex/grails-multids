f547c9f3faa1693d1e8ac5be574218becda57628
lucastex <lucas@blanq.com.br>
2017-04-12 21:59:46 -0300

    removed - initial inserts

--- grails-app/init/multids/BootStrap.groovy
+++ grails-app/init/multids/BootStrap.groovy
@@ -3,11 +3,6 @@ package multids
 class BootStrap {
 
     def init = { servletContext ->
-
-        new Contact(name: "Big Ben Roethlisberger").save()
-        new Contact(name: "Odell Beckham Jr").save()
-        new Contact(name: "Aaron Rodgers").save()
-
     }
     def destroy = {
     }
